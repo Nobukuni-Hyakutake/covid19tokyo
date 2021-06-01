7007eae2789899f57b7025c70f433c66343a6236
百武伸国 <>
2021-06-01 09:19:43 +0900

    group_code, sount_sum, date, last_day, yesterday

--- docs/covid19tokyo.py
+++ docs/covid19tokyo.py
@@ -6,11 +6,19 @@ from datetime import timedelta
 df01 = pd.read_csv('130001_tokyo_covid19_positive_cases_by_municipality.csv', encoding="UTF-8")
 df01['date']=pd.to_datetime(df01['公表_年月日'],
                format='%Y-%m-%d').dt.date
-last_day=df01[['date']].max()[0]
-print(last_day)
-df01['yesterday']=df01['date']-timedelta(1)
-###ここで７日前、14日前の列も加える。
-##SettingWithCopyWarningを回避のため、先にこれらの値を計算。あとで市区町村絞り込みする。 回避の解説はここ。https://linus-mk.hatenablog.com/entry/2019/02/02/200000
-df02=df01.query('集計区分=="市区町村"')
-print(df02)
-df02.to_csv('covid19tokyo_preprocessed.csv')
+last_day1=df01[['date']].max()[0]
+print(last_day1)
+##SettingWithCopyWarningを回避のため、copyとする。解説はここ。https://linus-mk.hatenablog.com/entry/2019/02/02/200000
+df02=df01.loc[(df01['集計区分']=='市区町村'),:].copy()
+df03=df02.rename(columns={'全国地方公共団体コード':'group_code','陽性者数':'count_sum'})
+df04=df03.loc[:,['group_code','count_sum','date']]
+
+df04['last_day']=last_day1
+df04['yesterday']=df04['date']-timedelta(1)
+
+#df02['sevendays_before']=df02['date']-timedelta(7)
+#df02['fourteendays_before']=df02['date']-timedelta(14)
+
+
+#df02.to_csv('covid19tokyo_preprocessed.csv')
+print(df04)
