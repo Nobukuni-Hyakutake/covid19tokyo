6b96a5e01b99d20293aea22c12d6b61e28fe3376
百武伸国 <>
2021-06-02 05:33:06 +0900

    実行ファイルをcovid19tokyoに統合

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
 # covid19tokyo
 
 解説：
-最新の
-https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_positive_cases_by_municipality.csv
-をコピーしてきてから、
-mitaka_musashino_easy.pyを実行すると
-mitaka_easy.html　と　musashino_easy.html　の人数が更新されるように作ってあります。
-
-covid19tokyo.pyは未だ開発中です。
+covid19tokyo.pyを実行すると
+mitaka_easy.html　と　musashino_easy.html と covid19tokyo_preprocessed.csvが出力されるように作ってあります。
--- docs/covid19tokyo.py
+++ docs/covid19tokyo.py
@@ -1,10 +1,12 @@
-# Tableau prep builderでやっている前処理をしてCSV出力までを目指す。
 # -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
 from datetime import datetime
 from datetime import timedelta
-df01 = pd.read_csv('130001_tokyo_covid19_positive_cases_by_municipality.csv', encoding="UTF-8")
+Print('Processing...')
+url ="https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_positive_cases_by_municipality.csv"
+df01 = pd.read_csv(url, encoding="UTF-8")
+
 df01['date']=pd.to_datetime(df01['公表_年月日'],
                format='%Y-%m-%d').dt.date
 last_day1=df01[['date']].max()[0]
@@ -49,3 +51,32 @@ out=df11
 print(out)
 print(out.dtypes)
 out.to_csv('covid19tokyo_preprocessed.csv')
+
+#やさしいにほんごここから
+
+last_week=last_day1-timedelta(7)
+df21=df01.loc[:,['市区町村名','陽性者数','date']]
+df_last_day =df21.query('date==@last_day')
+df_last_day_mitaka=df_last_day.query('市区町村名=="三鷹市"')
+last_day_mitaka_count=df_last_day_mitaka[['陽性者数']].mean()[0]
+df_last_day_musashino=df_last_day.query('市区町村名=="武蔵野市"')
+last_day_musashino_count=df_last_day_musashino[['陽性者数']].mean()[0]
+df_last_week =df21.query('date==@last_week')
+df_last_week_mitaka=df_last_week.query('市区町村名=="三鷹市"')
+last_week_mitaka_count=df_last_week_mitaka[['陽性者数']].mean()[0]
+df_last_week_musashino=df_last_week.query('市区町村名=="武蔵野市"')
+last_week_musashino_count=df_last_week_musashino[['陽性者数']].mean()[0]
+mitaka_7day_count=int(last_day_mitaka_count-last_week_mitaka_count)
+musashino_7day_count=int(last_day_musashino_count-last_week_musashino_count)
+mitaka_easy='<html><meta charset="UTF-8"><head><title>東京都 三鷹市 新型コロナウイルス陽性者数 (やさしいにほんご)</title></head><body style="font-size: 24px;" bgcolor="#ffffcc">とうきょうと　<b>みたかし</b><br><b>'+str(last_day.month)+'がつ'+str(last_day.day)+'にち</b><br>までのいっしゅうかんに<br>しんがたころなういるすに<br>かんせんしたひとは<br><b>'+str(mitaka_7day_count)+'</b>にんです</body></html>'
+fo=open('mitaka_easy.html','wt')
+fo.write(mitaka_easy)
+fo.close
+musashino_easy='<html><meta charset="UTF-8"><head><title>東京都 武蔵野市 新型コロナウイルス陽性者数 (やさしいにほんご)</title></head><body style="font-size: 24px;" bgcolor="#ffffcc">とうきょうと　<b>むさしのし</b><br><b>'+str(last_day.month)+'がつ'+str(last_day.day)+'にち</b><br>までのいっしゅうかんに<br>しんがたころなういるすに<br>かんせんしたひとは<br><b>'+str(musashino_7day_count)+'</b>にんです</body></html>'
+f2=open('musashino_easy.html','wt')
+f2.write(musashino_easy)
+f2.close
+
+#やさしいにほんごここまで
+
+print('Done')
