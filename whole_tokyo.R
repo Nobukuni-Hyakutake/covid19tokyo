34b03b81d1dea3f45bfb48c79f595dbb490bc711
百武伸国 <>
2022-07-14 16:52:58 +0900

    update data

--- whole_tokyo.R
+++ whole_tokyo.R
@@ -8,7 +8,7 @@ options(timeout=120)
 # delete # of download.file when updating data (need 90 approx. sec.)
 download.file("https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_patients_2022-1.csv", "130001_tokyo_covid19_patients.csv", "auto")
 # setting of latest data (no need to delete)
-ttoday<-tibble(date=c("2022-03-26"),cases=c(7440))
+ttoday<-tibble(date=c("2022-07-14"),cases=c(16662))
 # /setting of latest data
 ttoday$date<-ymd(ttoday$date)
 t01<-read_csv(file="130001_tokyo_covid19_patients.csv",col_names = FALSE,skip=1)
